c0da9e3926dff036c7bbf20d5948fc5d74dccb54
Silvia Donaire <silviadonaireserrano@gmail.com>
2024-02-10 00:04:12 +0100

    feat: add create app

--- app.py
+++ app.py
@@ -4,38 +4,40 @@ from flask_sqlalchemy import SQLAlchemy
 import joblib
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///respuestas.db'
-db = SQLAlchemy(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///respuestas.db'
+    # postgresql://flaskapp_memx_user:wP2ScRENooSz2GNJCnQAHvAXGHQj8qhc@dpg-cn3anrljm4es73bk3tig-a.frankfurt-postgres.render.com/flaskapp_memx
+    db = SQLAlchemy(app)
 
-class Respuesta(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    pregunta = db.Column(db.String(255))
-    respuesta = db.Column(db.Integer)
+    class Respuesta(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        pregunta = db.Column(db.String(255))
+        respuesta = db.Column(db.Integer)
 
-# Carga el modelo desde el archivo .pkl
-modelo = joblib.load('model/entrenamiento_estudiantes.pkl')
+    # Carga el modelo desde el archivo .pkl
+    modelo = joblib.load('model/entrenamiento_estudiantes.pkl')
 
-@app.route('/submit-cuestionario', methods=['POST'])
-def submit_cuestionario():
-    data = request.get_json()
+    @app.route('/submit-cuestionario', methods=['POST'])
+    def submit_cuestionario():
+        data = request.get_json()
 
-    respuestas_usuario = [respuesta for _, respuesta in zip(data['columnas'], data['respuestas'])]
-    # print("Respuestas para el modelo" + respuestas_usuario)
-    # Realiza la predicción con el modelo
-    prediccion = modelo.predict([respuestas_usuario])
+        respuestas_usuario = [respuesta for _, respuesta in zip(data['columnas'], data['respuestas'])]
+        # print("Respuestas para el modelo" + respuestas_usuario)
+        # Realiza la predicción con el modelo
+        prediccion = modelo.predict([respuestas_usuario])
 
-    for pregunta, respuesta in zip(data['columnas'], data['respuestas']):
-        nueva_respuesta = Respuesta(pregunta=pregunta, respuesta=respuesta)
-        db.session.add(nueva_respuesta)
+        for pregunta, respuesta in zip(data['columnas'], data['respuestas']):
+            nueva_respuesta = Respuesta(pregunta=pregunta, respuesta=respuesta)
+            db.session.add(nueva_respuesta)
 
-    db.session.commit()
+        db.session.commit()
 
-    return jsonify({'mensaje': 'Respuestas almacenadas correctamente', 'prediccion': prediccion.tolist()})
+        return jsonify({'mensaje': 'Respuestas almacenadas correctamente', 'prediccion': prediccion.tolist()})
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    if __name__ == '__main__':
+        with app.app_context():
+            db.create_all()
+        app.run(debug=True)
